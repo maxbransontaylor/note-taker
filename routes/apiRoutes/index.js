3fa4dfc986e2a321b7165e0087d44e66772ae5f2
Max Taylor <maxbransontaylor@gmail.com>
2022-08-13 19:40:25 -0400

    added all GET routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,3 +1,6 @@
 const router = require("express").Router();
-router.get("");
+const notes = require("../../db/db.json");
+router.get("/notes", (req, res) => {
+  res.json(notes);
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
 const express = require("express");
 const app = express();
 const fs = require("fs");
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 app.listen(PORT, () => {
   console.log(`server live on ${PORT}`);
