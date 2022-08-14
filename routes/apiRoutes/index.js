08460c28cde605f0cb38f09a9ddd6994eee19d29
Max Taylor <maxbransontaylor@gmail.com>
2022-08-13 20:19:57 -0400

    added DELETE functionality

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,15 +2,36 @@ const uuid = require("uuid");
 const fs = require("fs");
 const path = require("path");
 const router = require("express").Router();
+const notes = require("../../db/db.json");
 router.get("/notes", (req, res) => {
-  const notes = require("../../db/db.json");
   res.json(notes);
 });
 router.post("/notes", (req, res) => {
-  const notesArr = require("../../db/db.json");
-  req.body.id = uuid.v4();
-  console.log(req.body);
-  notesArr.push(req.body);
+  if (req.body.title && req.body.text) {
+    const notesArr = notes;
+    req.body.id = uuid.v4();
+    notesArr.push(req.body);
+    fs.writeFileSync(
+      path.join(__dirname + "../../../db/db.json"),
+      JSON.stringify(notesArr),
+      (err) => {
+        if (err) throw err;
+      }
+    );
+    res.json(req.body);
+  } else {
+    res.send("note needs title and text!");
+  }
+});
+router.delete("/notes/:id", (req, res) => {
+  const id = req.params.id;
+  const notesArr = notes;
+  for (let i = 0; i < notesArr.length; i++) {
+    if (notesArr[i].id === id) {
+      notesArr.splice(i, 1);
+      break;
+    }
+  }
   fs.writeFileSync(
     path.join(__dirname + "../../../db/db.json"),
     JSON.stringify(notesArr),
@@ -18,6 +39,6 @@ router.post("/notes", (req, res) => {
       if (err) throw err;
     }
   );
-  res.json(req.body);
+  res.send("note deleted");
 });
 module.exports = router;
